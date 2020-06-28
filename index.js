9925112ed4aa717ed65ba17a2d9b478b42e94532
Timur <bagirov.timur@gmail.com>
2020-06-28 23:03:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("keypress", function (e) {
+    if(e.code == "KeyA"){
+        let keyA = new Audio("./media/A.mp3");
+        keyA.play();
+    } else if (e.code == "KeyS"){
+        let keyS = new Audio("./media/S.mp3");
+        keyS.play();
+    } else if (e.code == "KeyD"){
+        let keyD = new Audio("./media/D.mp3");
+        keyD.play();
+    } else if (e.code == "KeyF"){
+        let keyF = new Audio("./media/F.mp3");
+        keyF.play();
+    } else if (e.code == "KeyG"){
+        let keyG = new Audio("./media/G.mp3");
+        keyG.play();
+    } else if (e.code == "KeyH"){
+        let keyH = new Audio("./media/H.mp3");
+        keyH.play();
+    } else if (e.code == "KeyJ"){
+        let keyJ = new Audio("./media/J.mp3");
+        keyJ.play();
+    } else if (e.code == "KeyE"){
+        let keyE = new Audio("./media/E.mp3");
+        keyE.play();
+    } else if (e.code == "KeyT"){
+        let keyT = new Audio("./media/T.mp3");
+        keyT.play();
+    } else if (e.code == "KeyU"){
+        let KeyU = new Audio("./media/U.mp3");
+        KeyU.play();
+    } else if (e.code == "KeyW"){
+        let KeyW = new Audio("./media/W.mp3");
+        KeyW.play();
+    } else if (e.code == "KeyY"){
+        let KeyY = new Audio("./media/Y.mp3");
+        KeyY.play();
+    }
+})
